b9fcf192086bb6bed4f06f9987e2b10ea215efc3
Rodriu12 <rodrigo.alarcon2301@alumnos.ubiobio.cl>
2025-12-08 22:20:58 -0300

    Se agregó una opción para parsear roles

--- frontend/src/components/ProfileCard.jsx
+++ frontend/src/components/ProfileCard.jsx
@@ -1,6 +1,13 @@
 import "@styles/profile.css";
 import profilePic from "@assets/profilePic.jpg";
 
+const parseRole = (role) => {
+  if (!role || typeof(role) !== "string") {
+    return "Rol desconocido";
+  }
+  return role.toUpperCase().replaceAll("_", " ");
+}
+
 const ProfileCard = ({ user }) => {
   return (
     <div className="profile-card">
@@ -17,7 +24,7 @@ const ProfileCard = ({ user }) => {
             <strong>Correo:</strong> {user.email}
           </p>
           <p>
-            <strong>Rol:</strong> {user.role}
+            <strong>Rol:</strong> {parseRole(user.role)}
           </p>
         </div>
       </div>
