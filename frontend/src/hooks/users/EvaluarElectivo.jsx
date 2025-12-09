4a68ba5088c7bbe3a6fccf70d5020178971e3db4
Rodriu12 <rodrigo.alarcon2301@alumnos.ubiobio.cl>
2025-12-08 22:25:29 -0300

    Se reemplazó EvaluarElectivo2 por EvaluarElectivo.jsx

--- frontend/src/hooks/users/EvaluarElectivo.jsx
+++ frontend/src/hooks/users/EvaluarElectivo.jsx
@@ -1,115 +1,64 @@
-/* import {aprobarElectivos, rechazarElectivos } from "../../services/electivo.service";
+import { aprobarElectivos, rechazarElectivos } from "../../services/electivo.service.js";
 import Swal from "sweetalert2";
 
-async function EvaluarElectivoInfo(electivos) {
-  const { value: formValues } = await Swal.fire({
-    title: "Editar Electivo",
-    html: `
-    <div>
-      <label for="swal2-input0">Nombre de electivo</label>  
-      <button >
-    </div>
-        `,
-    focusConfirm: false,
-    showCancelButton: true,
-    confirmButtonText: "Editar",
-    preConfirm: () => {
-      const nombre = document.getElementById("swal2-input0").value;
-      const descripcion = document.getElementById("swal2-input1").value;
-      const profesor = document.getElementById("swal2-input2").value;
-      const cupos = document.getElementById("swal2-input3").value;
-      const creditos = document.getElementById("swal2-input4").value;
-
-      console.log("NOMBRE: " + String(nombre));
-      console.log("DESCRIPCION: " + String(descripcion));
-      console.log("PROFESOR: " + String(profesor));
-      console.log("CUPOS: " + String(cupos));
-      console.log("CREDITOS: " + String(creditos));
-
-      if (!nombre || !profesor) {
-        Swal.showValidationMessage("Por favor, completa todos los campos");
-        return false;
-      }
-
-      if (!descripcion) {
-        Swal.showValidationMessage("La descripción es obligatoria");
-        return false;
-      }
-
-      if (nombre.length < 3 || nombre.length > 10) {
-        Swal.showValidationMessage(
-          "Ingrese el nombre del electivo a impartir"
-        );
-        return false;
-      }
-
-      if (!/^[a-zA-Z0-9_]+$/.test(nombre)) {
-        Swal.showValidationMessage(
-          "El nombre de usuario solo puede contener letras, números y guiones bajos"
-        );
-        return false;
-      }
-
-      if (!profesor || profesor.length < 5 || profesor.length > 10) {
-        Swal.showValidationMessage(
-          "Ingrese el nombre del profesor"
-        );
-        return false;
-      }
-
-      if (!/^[a-zA-Z0-9._%+-]+$/.test(profesor)) {
-        Swal.showValidationMessage(
-          "Por favor, ingresa un profesor con nombre válido"
-        );
-        return false;
-      }
-
-      if (cupos == 0 || cupos > CANTIDAD_MAXIMA_DE_CUPOS){
-        Swal.showValidationMessage(
-          "Ingrese los cupos del electivo (máximo 60):"
-        );
-      }else if(isNaN(cupos)){
-        Swal.showValidationMessage("Por favor ingrese un número de cupos válido");
-      }
-
-      if (creditos == 0 ||  creditos > CANTIDAD_MAXIMA_DE_CREDITOS){
-        Swal.showValidationMessage(
-          "Ingrese los créditos correspondientes al ramo (máximo 7):"
-        );
-      }else if(isNaN(creditos)){
-        Swal.showValidationMessage("Por favor ingrese un número válido de créditos");
-      }
-
-      return { nombre, descripcion, profesor, cupos, creditos };
-    },
-  });
-  if (formValues) {
-    return {
-      nombre: formValues.nombre,
-      descripcion: formValues.descripcion,
-      profesor: formValues.profesor,
-      cupos: formValues.cupos,
-      creditos: formValues.creditos
-    };
-  }
+const statusIsPositive = (status) => {
+    return (status >= 200 && status <= 299);
 }
 
-export const EditElectivo = (fetchElectivos) => {
-  const handleEditElectivo = async (electivoId, electivos) => {
-    try {
-      const formValues = await EvaluarElectivoInfo(electivos);
-      if (!formValues) return;
-
-      const response = await editElectivo(electivoId, formValues);
-      if (response) {
-        await fetchElectivos();
-      }
-    } catch (error) {
-      console.error("Error al editar electivo:", error);
+const swalCreationHelper = (isPositive, title, description) => {
+    let defaultIcon = 'error';
+    if (isPositive) {
+        defaultIcon = 'success';
     }
-  };
-  return { handleEditElectivo };
+    return Swal.fire({
+        title: title || 'Error!',
+        text: description || 'Error desconocido',
+        icon: defaultIcon || 'error'
+    });
+}
+
+const errorGenerico = (verbo) => {
+    return swalCreationHelper(false, "Error", `Error al ${verbo} electivo`);
+}
+
+export const AprobarElectivo = (fetchElectivos) => {
+    const handleAprobarElectivo = async (electivoId) => {
+        try {
+            const response = await aprobarElectivos(electivoId);
+            if (response) {
+                await fetchElectivos();
+                const isPositive = statusIsPositive(response.code);
+                swalCreationHelper(isPositive, isPositive ? 'Éxito' : 'Error', response.data.message);
+            } else {
+                errorGenerico('aprobar');
+            }
+            //console.log(JSON.stringify(response));
+            
+        } catch (error) {
+            errorGenerico('aprobar');
+            console.error("Error al aprobar electivo:", error);
+        }
+    };
+    return { handleAprobarElectivo };
 };
 
-export default EditElectivo;
-*/
+export const RechazarElectivo = (fetchElectivos) => {
+    const handleRechazarElectivo = async (electivoId) => {
+        try {
+            const response = await rechazarElectivos(electivoId);
+            if (response) {
+                await fetchElectivos();
+                const isPositive = statusIsPositive(response.code);
+                swalCreationHelper(isPositive, isPositive ? 'Éxito' : 'Error', response.data.message);
+            } else {
+                errorGenerico('rechazar');
+            }
+        } catch (error) {
+            console.error("Error al aprobar electivo:", error);
+            errorGenerico('rechazar');
+        }
+    };
+    return { handleRechazarElectivo };
+};
+
+export default AprobarElectivo;
