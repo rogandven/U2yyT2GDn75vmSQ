442f9fc9c7fc06707ccff58851983598914f8550
Roger Andrés Venegas Opazo <127449516+rogandven@users.noreply.github.com>
2025-12-20 14:51:40 -0300

    Added registerPrivate

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,6 +1,6 @@
 "use strict";
 import { Router } from "express";
-import { getUsers, getUserById, getProfile, updateUserById, deleteUserById } from "../controllers/user.controller.js";
+import { getUsers, getUserById, getProfile, updateUserById, deleteUserById, registerPrivate } from "../controllers/user.controller.js";
 import { authenticateJwt } from "../middleware/authentication.middleware.js";
 import { isAdmin } from "../middleware/authorization.middleware.js";
 
@@ -20,5 +20,6 @@ router.get("/", getUsers);
 router.get("/:id", getUserById);
 router.put("/:id", updateUserById);
 router.delete("/:id", deleteUserById);
+router.post("/", registerPrivate);
 
 export default router;
