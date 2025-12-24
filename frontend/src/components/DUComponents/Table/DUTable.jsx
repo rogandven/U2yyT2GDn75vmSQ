e600d7add8f8bebca84776c4b5f53dfb53bca1fc
Roger Andrés Venegas Opazo <127449516+rogandven@users.noreply.github.com>
2025-12-24 11:13:42 -0300

    WIP

--- frontend/src/components/DUComponents/Table/DUTable.jsx
+++ frontend/src/components/DUComponents/Table/DUTable.jsx
@@ -8,7 +8,7 @@ export const DUUserTable = (usuarios, handleDeleteUser, handleEditUser) => {
 
     let numero = 1;
     return Array.isArray(usuarios) && (
-        <div className="overflow-x-auto rounded-box border border-base-content/5 bg-base-100 max-w-11/12 m-3">
+        <div className="overflow-x-auto rounded-box border border-base-content/5 bg-base-100 m-3 max-h-full">
         <table className="table">
             <thead>
             <tr>
@@ -29,13 +29,13 @@ export const DUUserTable = (usuarios, handleDeleteUser, handleEditUser) => {
                 return (
                 <tr>
                     <th>{String(numero++)}</th>
-                    <td>{coalesceData(String(usuario && usuario.rut)) || "12345678-9"}</td>
-                    <td>{coalesceData(String(usuario && usuario.fullname)) || "Juanito Pérez"}</td>
-                    <td>{coalesceData(String(usuario && usuario.username)) || "juanitoperez21"}</td>
-                    <td>{coalesceData(String(usuario && usuario.email)) || "juanitoperez@email.com"}</td>
+                    <td>{coalesceData(String(usuario && usuario.rut)) || "N/A"}</td>
+                    <td>{coalesceData(String(usuario && usuario.fullname)) || "N/A"}</td>
+                    <td>{coalesceData(String(usuario && usuario.username)) || "N/A"}</td>
+                    <td>{coalesceData(String(usuario && usuario.email)) || "N/A"}</td>
                     <td>{coalesceData(String(usuario && usuario.role).toUpperCase().replaceAll("_", " ")) || "usuario"}</td>
-                    <td>{coalesceData(String(usuario && usuario.id_carrera)) || "1"}</td>
-                    <td>{coalesceData(String(usuario && usuario.creditos)) || "0"}</td>
+                    <td>{coalesceData(String(usuario && usuario.id_carrera)) || "N/A"}</td>
+                    <td>{coalesceData(String(usuario && usuario.creditos)) || "N/A"}</td>
                     <td>
                         <button class="btn btn-primary m-1" onClick={() => {handleEditUser(usuario.id, usuario)}}>Editar</button>
                         <button class="btn btn-secondary m-1" onClick={() => {handleDeleteUser(usuario.id, usuario)}}>Eliminar</button>
