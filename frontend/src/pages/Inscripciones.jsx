cd0ed71a32fee43bf3056d3edcd5539409439169
Roger Andrés Venegas Opazo <127449516+rogandven@users.noreply.github.com>
2025-12-26 11:58:46 -0300

    More renaing

--- frontend/src/pages/Inscripciones.jsx
+++ frontend/src/pages/Inscripciones.jsx
@@ -13,7 +13,7 @@ import useCreateInscripcion from "../hooks/Inscripciones/useCreateInscripcion.js
 // import useCreateInscripcion from "../hooks/Inscripciones/useCreateInscripcion.jsx";
 import useEditInscripcion from "../hooks/Inscripciones/useEditInscripcion.jsx";
 import useDeleteInscripcion from "../hooks/Inscripciones/useDeleteInscripcion.jsx";
-import useChangeInscripcionStatus from "@/hooks/Inscripciones/useChangeInscripcionStatus.jsx";
+import useChangeInscripcionStatus from "@hooks/Inscripciones/useChangeInscripcionStatus.jsx";
 
 import { useGetElectivoNames } from "../hooks/Inscripciones/useGetNames.jsx";
 import { useGetUserNames } from "../hooks/Inscripciones/useGetNames.jsx";
