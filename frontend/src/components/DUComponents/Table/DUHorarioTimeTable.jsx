653af3327d9ceab618dc8366be97ec2c8b55c17f
Roger Andrés Venegas Opazo <127449516+rogandven@users.noreply.github.com>
2025-12-26 00:47:42 -0300

    WIP

---
+++ frontend/src/components/DUComponents/Table/DUHorarioTimeTable.jsx
@@ -0,0 +1,64 @@
+import { FaEdit } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
+
+const mostrarHorarios = (data, handleEditTimetable, handleDeleteTimetable) => {
+  if (Array.isArray(data) && data.length > 0) {
+      return data.map((Timetable) => (
+                  <tr key={"Timetable-"+Timetable.id_horario}>
+                      <td>{Timetable.id_horario}</td>
+                      <td>{Timetable.id_electivo}</td>
+                      <td>{Timetable.hora_inicio}</td>
+                      <td>{Timetable.hora_termino}</td>
+                      <td>
+                        <div className="badge badge-primary">
+                          {Timetable.sala}
+                        </div>
+                      </td>
+                      <td>
+                        <div className="badge badge-secondary">
+                          {String(Timetable.dia).toUpperCase()}
+                        </div>
+                      </td>
+                      <td>
+                      <button className="btn btn-primary m-1" onClick={() => {handleEditTimetable(Timetable.id_horario, Timetable)}}><FaEdit></FaEdit></button>
+                      <button className="btn btn-secondary m-1" onClick={() => {handleDeleteTimetable(Timetable.id_horario)}}><MdDelete></MdDelete></button>
+                      </td>
+                  </tr>
+      ));
+  } else {
+      return (
+          <tr>
+              <td colSpan="7">No hay horarios disponibles.</td>
+          </tr>
+      )
+  }
+}
+
+export const DUHorarioTable = ({data, handleEditTimetable, handleDeleteTimetable}) => {
+    /* const coalesceData = (data) => {
+        if (data === null || data === "null" || data === undefined || data === "undefined") {
+            return "";
+        }
+        return data;
+    } */
+    return (
+        <div className="overflow-x-auto rounded-box border border-base-content/5 bg-base-100 m-3 max-h-full">
+        <table className="table">
+            <thead>
+            <tr>
+                <th></th>
+                <th>Electivo</th>
+                <th>Hora Inicio</th>
+                <th>Hora Término</th>
+                <th>Sala</th>
+                <th>Día</th>
+                <th>Acciones</th>             
+            </tr>
+            </thead>
+            <tbody>
+              {mostrarHorarios(data, handleEditTimetable, handleDeleteTimetable)}
+            </tbody>
+        </table>
+        </div>
+    ); 
+}
--- frontend/src/pages/Timetable.jsx
+++ frontend/src/pages/Timetable.jsx
@@ -5,76 +5,16 @@ import editTimetable from "@hooks/timetable/useUpdateTimetable.jsx";
 import deleteTimetable from "@hooks/timetable/useDeleteTimetable.jsx";
 import { useEffect } from "react";
 import { useState } from "react";
+import { DUHorarioTable } from "../components/DUComponents/Table/DUHorarioTimetable.jsx";
 
 const Timetable = () => {
-    const mostrarHorarios = (data) => {
-        if (Array.isArray(data) && data.length > 0) {
-            return data.map((Timetable) => (
-                        <tr key={"Timetable-"+Timetable.id_horario}>
-                            <td>{Timetable.id_horario}</td>
-                            <td>{Timetable.id_electivo}</td>
-                            <td>{Timetable.hora_inicio}</td>
-                            <td>{Timetable.hora_termino}</td>
-                            <td>{Timetable.sala}</td>
-                            <td>{Timetable.dia}</td>
-                            <td>
-                                <button onClick={() => handleEditTimetable(Timetable.id_horario, Timetable)}>Editar</button>
-                                <button onClick={() => handleDeleteTimetable(Timetable.id_horario)}>Eliminar</button>
-                            </td>
-                        </tr>
-            ));
-        } else {
-            return (
-                <tr>
-                    <td colSpan="7">No hay horarios disponibles.</td>
-                </tr>
-            )
-        }
-    }
-
     const [horarioData, setHorarioData] = useState([]);
-    // console.log(horarioData);
-    // console.log(setHorarioData);
-
-    // console.log(useGetTimetable);
-    // const [horarioData, setHorarioData] = useState([]);
-    /* console.log(useGetTimetable);
-    console.log(typeof(useGetTimetable));
-    try {
-        console.log(JSON.stringify(useGetTimetable));
-    } catch (error) {
-        console.error("Error al convertir useGetTimetable a JSON:", error);
-    } */
-
-
 
     const [timetables, fetchTimetable] = useGetTimetable(horarioData, setHorarioData);
-    /* console.log(timetables);
-    console.log(typeof(timetables));
-    try {
-        console.log(JSON.stringify(timetables));
-    } catch (error) {
-        console.error("Error al convertir timetables a JSON:", error);
-    }
-    console.log(fetchTimetable);
-    console.log(typeof(fetchTimetable));
-    try {
-        console.log(JSON.stringify(fetchTimetable));
-    } catch (error) {
-        console.error("Error al convertir fetchTimetable a JSON:", error);
-    } */
 
     const { handleCreateTimetable } = useCreateTimetable(fetchTimetable);
     const { handleEditTimetable } = editTimetable(fetchTimetable);
     const { handleDeleteTimetable } = deleteTimetable(fetchTimetable);
-    /* console.log(fetchTimetable);
-    console.log(typeof(fetchTimetable));
-    try {
-        console.log(JSON.stringify(fetchTimetable));
-    } catch (error) {
-        console.error("Error al convertir fetchTimetable a JSON:", error);
-    } */
-    
 
     useEffect(() => {
         if (typeof(fetchTimetable) === 'function') {
@@ -84,24 +24,8 @@ const Timetable = () => {
 
     return (
         <div className="timetable-page">
-            <h2>Lista de horarios</h2>
-            <button className="create" onClick={() => handleCreateTimetable(Timetable.hora_inicio,Timetable.hora_termino,Timetable.sala,Timetable.dia)}>Crear Horario</button>
-            <table className="timetable-table">
-                <thead>
-                     <tr>
-                        <th>ID</th>
-                        <th>codigo electivo</th>
-                        <th>Hora de Inicio</th>
-                        <th>Hora de Término</th>
-                        <th>Sala</th>
-                        <th>Día</th>
-                        <th>Acciones</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {mostrarHorarios(timetables.data)}
-                </tbody>
-            </table>
+            <button className="create btn btn-primary ml-3 mt-3 mb-0" onClick={() => handleCreateTimetable(Timetable.hora_inicio,Timetable.hora_termino,Timetable.sala,Timetable.dia)}>Crear Horario</button>
+            <DUHorarioTable data={timetables?.data || []} handleEditTimetable={handleEditTimetable} handleDeleteTimetable={handleDeleteTimetable} />
         </div>
     );
 };
