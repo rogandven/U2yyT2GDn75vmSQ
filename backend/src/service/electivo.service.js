4f0419ce1e88d265316c10a339dee65ea39ae54e
Roger Andrés Venegas Opazo <127449516+rogandven@users.noreply.github.com>
2025-12-26 08:25:18 -0300

    touppercase

--- backend/src/service/electivo.service.js
+++ backend/src/service/electivo.service.js
@@ -14,7 +14,7 @@ const processElectivoArray = async (resultados) => {
       for (let i = 0; i < resultados.length; i++) {
         try {
           current = await RAW_getUserById(resultados[i].id_profesor);
-          nombre_profesor = String((current && current.fullname) || "JUANITO PÉREZ");
+          nombre_profesor = String((current && current.fullname) || "JUANITO PÉREZ").toUpperCase();
           Object.assign(resultados[i], {nombre_profesor: nombre_profesor});
         } catch (error) {}
       }
