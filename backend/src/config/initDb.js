38dae3e8c4e7741e6827be2e4e6de4824dc60f4e
Roger Andrés Venegas Opazo <127449516+rogandven@users.noreply.github.com>
2025-12-27 19:22:56 -0300

    Added toUpperCase

--- backend/src/config/initDb.js
+++ backend/src/config/initDb.js
@@ -20,7 +20,7 @@ export async function createUsers() {
 
     const users = [
       {
-        fullname: "Roger Venegas",
+        fullname: "Roger Venegas".toUpperCase(),
         username: "rogandven",
         rut: "4825562-0",
         email: EXMAPLE_EMAIL_1,
@@ -31,7 +31,7 @@ export async function createUsers() {
         creditos: 0
       },
       {
-        fullname: "Sebastián Pinto",
+        fullname: "Sebastián Pinto".toUpperCase(),
         username: "seba",
         rut: "22849268-K",
         email: EXMAPLE_EMAIL_2,
@@ -42,7 +42,7 @@ export async function createUsers() {
         creditos: 0
       },
       {
-        fullname: "Carlos Domínguez",
+        fullname: "Carlos Domínguez".toUpperCase(),
         username: "carmanolo",
         rut: "20924430-6",
         email: EXMAPLE_EMAIL_3,
@@ -53,7 +53,7 @@ export async function createUsers() {
         creditos: 300
       },
       {
-        fullname: "Rodrigo Alarcón",
+        fullname: "Rodrigo Alarcón".toUpperCase(),
         username: "rodriser12",
         rut: "8347186-7",
         email: EXMAPLE_EMAIL_4,
@@ -64,7 +64,7 @@ export async function createUsers() {
         creditos: 300
       },
       {
-        fullname: "Fermín Millanao",
+        fullname: "Fermín Millanao".toUpperCase(),
         username: "fermin23",
         rut: "16057069-5",
         email: EXMAPLE_EMAIL_5,
@@ -75,7 +75,7 @@ export async function createUsers() {
         creditos: 0
       },
       {
-        fullname: "Andrés Opazo",
+        fullname: "Andrés Opazo".toUpperCase(),
         username: "andres123",
         rut: "4738683-7",
         email: EXMAPLE_EMAIL_6,
