5ed731bf720d47610f1efbeb778a2466a29afc22
Roger Andrés Venegas Opazo <127449516+rogandven@users.noreply.github.com>
2025-12-27 21:09:05 -0300

    Fixed date input

--- frontend/src/hooks/utils/swalField.jsx
+++ frontend/src/hooks/utils/swalField.jsx
@@ -7,11 +7,19 @@ export const createSwalField = (inputId, label, value) => {
     `
 }
 
+const getToday = () => {
+    try {
+        return (new Date(Date.now())).toISOString().split("T")[0];
+    } catch (error) {
+        return "1970-01-01";
+    }
+}
+
 export const createSwalDateField = (inputId, label, value) => {
     return `
     <label class="input m-1">
         <span class="label">${label}</span>
-        <input type="date" id="swal2-input${Number(inputId)}" value="${value || Date.prototype.toISOString(Date.now())}" />
+        <input type="date" id="swal2-input${Number(inputId)}" value="${value || getToday()}" />
     </label>
     `
 }
