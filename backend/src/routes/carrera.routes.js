8fe414d98d76da4e8bcee808bfc592415fdde3cf
Roger Andrés Venegas Opazo <127449516+rogandven@users.noreply.github.com>
2025-12-31 17:07:50 -0300

    Added carrera routes

---
+++ backend/src/routes/carrera.routes.js
@@ -0,0 +1,15 @@
+"use strict"; 
+import { Router } from "express";
+import { authenticateJwt } from "../middleware/authentication.middleware.js";
+import { createCarrera, deleteCarrera, getCarreraById, getCarreras, updateCarrera } from "../controllers/carrera.controller.js";
+
+const router = Router();
+
+router.use(authenticateJwt);
+router.post("/", createCarrera);
+router.get("/:id", getCarreraById);
+router.get("/", getCarreras);
+router.patch("/:id", updateCarrera);
+router.delete("/:id", deleteCarrera);
+
+export default router;
