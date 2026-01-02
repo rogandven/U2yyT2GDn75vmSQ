bf8430f4503bcc3317680fe79d238849a054c15a
Roger Andrés Venegas Opazo <127449516+rogandven@users.noreply.github.com>
2026-01-02 20:39:20 -0300

    Fixed typos

--- frontend/src/services/electivo.service.js
+++ frontend/src/services/electivo.service.js
@@ -713,43 +713,43 @@ const routeHelper2 = async (URL, body, axiosFunction) => {
   }
 };
 
-export async function getElectivos() {
+export async function getElectivos3() {
   return (await routeHelper("/electivos/get/", null, axios.get))?.data;
 }
 
-export async function getElectivoById(id) {
+export async function getElectivoById3(id) {
   return await routeHelper(`/electivos/get/${id}`, null, axios.get);
 }
 
-export async function getElectivosSinAprobar() {
+export async function getElectivosSinAprobar3() {
   return await routeHelper(`/electivos/get_private/`, null, axios.get);
 }
 
-export async function createElectivoProfesor(electivoData) {
+export async function createElectivoProfesor3(electivoData) {
   return await routeHelper("/electivos", electivoData, axios.post);
 }
 
-export async function createElectivoJefeDeCarrera(electivoData) {
+export async function createElectivoJefeDeCarrera3(electivoData) {
   return await routeHelper("/electivos/private", electivoData, axios.post);
 }
 
-export async function approveElectivo(id) {
+export async function approveElectivo3(id) {
   return await routeHelper(`/electivos/private/approve/${id}`, null, axios.post);
 }
 
-export async function rejectElectivo(id) {
+export async function rejectElectivo3(id) {
   return await routeHelper(`/electivos/private/reject/${id}`, null, axios.post);
 }
 
-export async function editElectivo(id, electivoData) {
+export async function editElectivo3(id, electivoData) {
   return await routeHelper(`/electivos/${id}`, electivoData, axios.patch);
 }
 
-export async function deleteElectivo(id) {
+export async function deleteElectivo3(id) {
   return await routeHelper(`/electivos/${id}`, null, axios.delete);
 }
 
-export async function FRONTEND_getElectivoList() {
+export async function FRONTEND_getElectivoList3() {
   const BASE_CASE = [];
   try {
     const response = await axios.get('/electivos/frontend_list/');
