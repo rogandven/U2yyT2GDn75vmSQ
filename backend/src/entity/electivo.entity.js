ff925561d9d29748132a43853e1493182a4b1065
Rodriu12 <rodrigo.alarcon2301@alumnos.ubiobio.cl>
2026-01-02 12:23:53 -0300

    hh

--- backend/src/entity/electivo.entity.js
+++ backend/src/entity/electivo.entity.js
@@ -233,14 +233,9 @@ export const ElectivoEntity = new EntitySchema({
       nullable: false,
     },
     motivo: {
-<<<<<<< Updated upstream
-      type: "text",
-    },
-=======
       type: String,
       nullable: true,
     }
->>>>>>> Stashed changes
   },
   checks: [
       { expression: `"estado" IN ${arrayDeStringAArrayDeSQL(ARRAY_ESTADOS_VALIDOS)}` },
